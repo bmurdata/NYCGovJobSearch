9969b8f20bcfd443e3b52cae0cc0388f2a1e88fe
bmurdata <32182553+bmcap@users.noreply.github.com>
2020-02-01 15:41:01 -0500

    Database access test

---
+++ dbtest.py
@@ -0,0 +1,18 @@
+import pyodbc 
+computer="computer host"
+computer=input("Enter Local Host for SQLExpress")
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server='+str(computer)+'\SQLEXPRESS;'
+                      'Database=NYCGovJob_Test;'
+                      'Trusted_Connection=yes;')
+
+cursor = conn.cursor()
+cursor.execute('SELECT @@version')
+for row in cursor:
+    print(row)
+
+cursor.execute("CREATE TABLE Ftable(ID int, Descrip varchar(20))")
+cursor.commit()
+
+cursor.execute("INSERT INTO Ftable (ID, Descrip) VALUES (1,'Hello World' )")
+cursor.commit()
