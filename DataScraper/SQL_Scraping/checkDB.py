3f1b3f2e266319a8d8706b6eafd3e40c2c60c125
bmurdata <bpmjoker@gmail.com>
2021-01-22 00:16:40 -0500

    Further updates to the databse comparision, final checks

--- DataScraper/SQL_Scraping/checkDB.py
+++ DataScraper/SQL_Scraping/checkDB.py
@@ -2,40 +2,9 @@ import db
 from db import engine
 from sqlalchemy.orm import sessionmaker
 from models.JobMeta import JobMeta_Model, AgencySearch_Model,JobDescrip_Model
-# Job Meta Class
-# class JobMeta(Base):
-#     __tablename__='job_info_short'
-
-#     jobNum = sa.Column(sa.Integer, primary_key=True)
-#     hiring_agency=sa.Column(sa.String(200))
-#     jobLink=sa.Column(sa.String(2083))
-#     job_ID=sa.Column(sa.Integer)
-#     business_title=sa.Column(sa.String(500))
-#     civil_title=sa.Column(sa.String(200))
-#     title_class=sa.Column(sa.String(100))
-#     job_category=sa.Column(sa.String(200))
-#     career_level=sa.Column(sa.String(100))
-#     work_location=sa.Column(sa.String(300))
-#     division_work_unit=sa.Column(sa.String(300))
-#     total_openings=sa.Column(sa.Integer)
-#     title_code=sa.Column(sa.String(100))
-#     level=sa.Column(sa.String(20))
-#     proposed_salary_range=sa.Column(sa.String(200))
-#     posted=sa.Column(sa.DateTime)
-#     post_until=sa.Column(sa.DateTime)
 
 Session = sessionmaker(bind=engine)
 session=Session()
-for row in session.query(JobMeta_Model,JobMeta_Model.hiring_agency).all():
-    print(row.JobMeta_Model.jobLink)
-
-for row in session.query(JobDescrip_Model,JobDescrip_Model.jobNum).all():
-    print(row.JobDescrip_Model.work_location)
-
-for row in session.query(AgencySearch_Model,AgencySearch_Model.jobNum).all():
-    print(row.AgencySearch_Model.shortCategory)  
-
-# SUCCESSSSSSS WITH SQLALCHEMY!!!! Now to work on the actual checker
 
 # Idea- Compare new JSON data to details database, where there is no match, scrape
 # for the details. Where the post_until is before today's date- remove it
@@ -45,31 +14,79 @@ import json
 import os
 from pathlib import Path
 dir_path=str(Path(os.path.dirname(os.path.realpath(__file__))).parents[0])+'/output/'
-print(dir_path)
-test='2021-01-21_1611269606By-AgencyCode.json'
 
+# joblinks must be a array of tuples- form of link, jobnum
+# Compare to find new details, remove old ones, return link,jobID
 
-with open(dir_path+test) as ifile:
-    try:
-        jobNumbers=[]
-        jsondata=json.load(ifile)
-        print("jsondata has a length of "+str(len(jsondata)))
-        for category in jsondata:
-            for job in jsondata[category]:
-                jobNumbers.append(job["jobNum"])
-        failure=False
+def compareJsonToCsv_from_db():
+    details_data=session.query(JobMeta_Model.jobLink,JobMeta_Model.jobNum).all()
+    agency_data=session.query(AgencySearch_Model.Link,AgencySearch_Model.jobNum).all()
 
-    except Exception as e:
-        print("Failed to load the data reason: \n"+str(e))
-        failure=True
-# jobNumbers=[457084,452082,456830]
-finalcomplete=[]
-print(len(jobNumbers))
-counter=0
-allJobNum=session.query(JobDescrip_Model.jobNum).filter(JobDescrip_Model.jobNum.in_((jobNumbers))).all()
+    print(len(details_data))
+    print(len(agency_data))
+    
+    joblinks=[]
+    counter=0
+    evilcount=0
+    details_jobNum=[i[1] for i in details_data]
+    agency_jobNum=[i[1] for i in agency_data]
+    print(len(agency_jobNum))
+    # Check for new ones. 
+    for agency_id in agency_jobNum:
+        
+        if agency_id in details_jobNum:
+            agency_data=[job for job in agency_data if job[1]!=agency_id]
+        else:
+            
+            # print(agency_id)
+            counter=counter+1
+    for detail_id in details_jobNum:
+        if detail_id in agency_jobNum:
+            continue
+        else:
+            evilcount=evilcount+1
+            x=[i for i in details_data if i[1]==detail_id]
+            print(x[0])
+            details_data.remove(x[0])
+    # Remove expired
+    # for detail_id in details_data:
+    #     if detail_id in agency_data:
+    #         continue
+    #     else:
+    #         evilcount=evilcount+1
+    #         details_data.remove(detail_id)
+    joblinks=agency_data
+    # Remove expired details
+    print(counter)
+    print(evilcount)
+    print(len(details_data))
+    return joblinks
 
-for row in session.query(JobDescrip_Model.jobNum).filter(JobDescrip_Model.jobNum.in_((jobNumbers))).all():
-    print(row.jobNum)
-    counter=counter+1
+print(len(compareJsonToCsv_from_db()))
+print(compareJsonToCsv_from_db())
+def compareJsonToCsv_from_file(jsonfile):
+    print(dir_path)
+    test='2021-01-21_1611269606By-AgencyCode.json'
 
-print(str(counter))
+    with open(dir_path+test) as ifile:
+        try:
+            jobNumbers=[]
+            jsondata=json.load(ifile)
+            print("jsondata has a length of "+str(len(jsondata)))
+            for category in jsondata:
+                for job in jsondata[category]:
+                    jobNumbers.append(job["jobNum"])
+            failure=False
+
+        except Exception as e:
+            print("Failed to load the data reason: \n"+str(e))
+            failure=True
+
+# counter=0
+# allJobNum=session.query(JobDescrip_Model.jobNum).filter(JobDescrip_Model.jobNum.in_((jobNumbers))).all()
+
+# for row in session.query(JobDescrip_Model.jobNum).filter(JobDescrip_Model.jobNum.in_((jobNumbers))).all():
+#     print(row.jobNum)
+#     counter=counter+1
+
+# print(str(counter))
